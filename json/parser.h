5329cbc26bd2df327767aef1737fc6e2497cbe9c
Parzin158 <139872682+Parzin158@users.noreply.github.com>
2023-08-07 18:00:42 +0800

    Update parser.h

--- json/parser.h
+++ json/parser.h
@@ -1,29 +1,24 @@
 #pragma once
-
+#include <functional>
 #include <string>
+#include <stdexcept>
 #include "json.h"
 using namespace std;
 
 namespace my{
 namespace json{
-
 class parser{
 public:
     parser();
-
     /* 加载解析的字符串 */
-    void load(const string & str); 
-    
+    void load(const string & str);     
     json parse();
 
 private:
-
     /* 跳过空白字符 */
     void skip_white_space();
-
     /* 获取下一个字符 */
-    char get_next_token();
-    
+    char get_next_token();    
     /* 解析值函数 */
     json parse_null();
     json parse_bool();
@@ -32,11 +27,8 @@ private:
     json parse_array();
     json parse_object();
 
-private:
-    string m_str; //存放数据
-    int m_idx; //存放索引
-
+    string m_str; // 存放数据
+    int m_idx; // 存放索引
 };
-
 }
 }
